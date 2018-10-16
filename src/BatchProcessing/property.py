700967d33e96df29a1b9e772c60c85262b654aa2
SainathDutkar <saidutkar@gmail.com>
2018-10-16 18:17:30 -0400

    Adding Spark Batch process job

--- src/BatchProcessing/property.py
+++ src/BatchProcessing/property.py
@@ -6,4 +6,4 @@ Created on Sat Oct 13 17:26:11 2018
 """
 
 path = "s3://crawlerdump092018/Wikidump10132018/*.json"
-dbUrl = "jdbc:mysql://saidutkar-db.c5xfxsiw32if.us-east-1.rds.amazonaws.com:3306/wikiStats""?user=SaiDutkarInsight&password=Sainath4321"
+dbUrl = "jdbc:mysql://********************.us-east-1.rds.amazonaws.com:3306/*******""?user=************&password=********"
